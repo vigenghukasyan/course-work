3d5d991715f1fa7def138605eae9d339779f9ca7
ᴠɪɢᴇɴ ɢʜᴜᴋᴀꜱʏᴀɴ <87125708+vigenghukasyan@users.noreply.github.com>
2024-02-13 00:08:10 +0400

    Deleted comment in matrix.cpp

--- matrix/matrix.cpp
+++ matrix/matrix.cpp
@@ -1,9 +1,3 @@
-﻿/* 
-Given a rectangular matrix of dimension N x M, whose elements are integers, display it on a video type screen.
-Create a program that will slowly form a vector whose elements are taken from the columns of the matrix that contain only positive numbers.
-Sort the vector in ascending order.
-*/
-
 #include <iostream>
 #include <ctime>
 #include <Windows.h>
